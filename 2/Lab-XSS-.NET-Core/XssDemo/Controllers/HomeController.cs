b181bf0e8bb74afabce5bc0985a3e3c15ab78630
Ph33rr <77519024+Ph33rr@users.noreply.github.com>
2021-10-09 10:52:45 +0300

    up

---
+++ 2/Lab-XSS-.NET-Core/XssDemo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using XssDemo.Models;
+
+namespace XssDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Customer());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Customer model)
+        {
+            return View(model);
+        }
+        
+        public IActionResult JavascriptAttack(int userId)
+        {
+            return View(new Customer{UserId= userId });
+        }
+
+        [HttpPost]
+        public IActionResult JavascriptAttack(Customer model)
+        {
+            return RedirectToAction("JavascriptAttack", new { userId = model.UserId });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
---
+++ 2/Lab-XSS-.NET-Core/XssDemo/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace XssDemo.Models
+{
+    public class Customer
+    {
+        public string UserName { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
